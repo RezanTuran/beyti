9412aa4c9e5ecc4f5db7d0747c7064e26ab6382e
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2021-11-03 15:26:30 +0100

    home page responsive test

--- src/components/restaurant-info/restaurant-info.tsx
+++ src/components/restaurant-info/restaurant-info.tsx
@@ -2,15 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 const RestaurantInfo = () => {
-  return (
-    <Grid
-      style={{
-        marginTop: '-1.5em',
-        height: '70vh',
-        backgroundColor: '#484848',
-      }}
-    ></Grid>
-  );
+  return <Grid></Grid>;
 };
 
 export default RestaurantInfo;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
 import heroVideo from '../../assets/backgroundVideo.mp4';
-import { Grid } from '@material-ui/core';
+import { Grid, useMediaQuery } from '@material-ui/core';
 //import useStyles from './styles';
 import RestaurantInfo from '../../components/restaurant-info';
+import mobileBack from '../../assets/mobile.jpg';
 
 const Home = () => {
+  const isMobile = useMediaQuery('(min-width:600px)');
+
   //const classes = useStyles();
   return (
     <Grid>
-      <ReactPlayer
-        url={heroVideo}
-        playing
-        loop
-        muted
-        controls={false}
-        height="auto"
-        width="100%"
-      />
+      {isMobile ? (
+        <ReactPlayer
+          url={heroVideo}
+          playing
+          loop
+          muted
+          controls={false}
+          height="auto"
+          width="100%"
+        />
+      ) : (
+        <Grid
+          style={{
+            backgroundImage: `url(${mobileBack})`,
+            height: '100vh',
+            backgroundSize: 'cover',
+          }}
+        ></Grid>
+      )}
       <RestaurantInfo />
     </Grid>
   );
